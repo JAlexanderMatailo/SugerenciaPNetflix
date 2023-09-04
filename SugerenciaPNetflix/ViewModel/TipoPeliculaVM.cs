262b8e27eb33861e2cdb0b3552c782396f8df411
JAlexanderMatailo <52136249+JAlexanderMatailo@users.noreply.github.com>
2023-09-03 22:58:20 -0500

    creacion de clases UsuariosVM y PeliuclasVM

--- SugerenciaPNetflix/ViewModel/TipoPeliculaVM.cs
+++ SugerenciaPNetflix/ViewModel/TipoPeliculaVM.cs
@@ -10,11 +10,23 @@
     #endregion
 
     #region Usuario
-
+    public class UsuarioVM
+    {
+        public int ID_Usuario { get; set; } // int Primary key identity NOT NULL,
+        public string Nombre_usuario { get; set; } // varchar(50) not null,
+        public DateTime fecha_nacimiento { get; set; } //date not null
+        public List<TipoPeliculaVM> tipos { get; set; }
+    }
     #endregion
 
     #region Pelicula
-
+    public class PeliculaVM
+    {
+        public int Id_Pelicula { get; set; } // int primary key identity not null,
+        public string nombre_pelicula { get; set; } //varchar(50) not null,
+        public string duracion_pelicula { get; set; } //varchar(50) not null
+        public List<TipoPeliculaVM> tipos_pelicula { get; set; }
+    }
     #endregion
 
 }
